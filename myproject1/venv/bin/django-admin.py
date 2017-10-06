1caf3a2fe039560154f46a1532614b9cd6cfe74e
Ryan Morrison <ryanam26@yahoo.com>
2017-10-05 22:29:38 -0400

    initial commit

---
+++ myproject1/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ryanmorrison/Desktop/Python_Development/myproject1/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
