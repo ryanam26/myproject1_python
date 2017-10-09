ee4904624d5f3479e7cdf05ecf415e87c87951b0
Ryan Morrison <ryanam26@yahoo.com>
2017-10-09 09:58:29 -0400

    complete part3

--- myproject1/myproject1/boards/tests.py
+++ myproject1/myproject1/boards/tests.py
@@ -10,6 +10,8 @@ from .models import Board, Topic, Post
 
 from django.contrib.auth.models import User
 
+from .forms import NewTopicForm
+
 
 class HomeTests(TestCase):
     def setUp(self):
@@ -106,14 +108,16 @@ class NewTopicTests(TestCase):
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
-    def test_new_topic_invalid_post_data(self):
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
         '''
         Invalid post data should not redirect
         The expected behavior is to show the form again with validation errors
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
@@ -129,6 +133,14 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+    def test_contains_form(self):  # new test
+    	url = reverse('new_topic', kwargs={'pk': 1})
+    	response = self.client.get(url)
+    	form = response.context.get('form')
+    	self.assertIsInstance(form, NewTopicForm)
+
+    
 
 
 
